586f6de22dc469121ce17748692ab477f398f5b9
Cristian Lopez <cristiandavid.lopez.ext@circontrol.com>
2025-01-23 02:03:55 +0100

    All genres page implemented

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -25,7 +25,7 @@ func (m *PostgresDBRepo) AllMovies(genre ...int) ([]*models.Movie, error) {
 
 	where := ""
 	if len(genre) > 0 {
-		where = fmt.Sprintf("where id in (select movie_id from movies_genres where genre_id = %d", genre[0])
+		where = fmt.Sprintf("where id in (select movie_id from movies_genres where genre_id = %d)", genre[0])
 	}
 	query := fmt.Sprintf(`
   select 
